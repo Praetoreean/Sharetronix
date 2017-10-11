570c45c98505b9fbf9feecca976817663e0dde97
Praetoreean <touche.sign@yahoo.com>
2017-10-11 10:49:25 +0300

    some change

--- system/languages/en/inside/footer.php
+++ system/languages/en/inside/footer.php
@@ -2,37 +2,37 @@
 	
 	$lang	= array
 	(
-		'ftrlinks_section_general'	=> 'Ссылки',
-		'ftrlinks_section_groups'	=> 'Группы',
-		'ftrlinks_section_findpeople'	=> 'Участники сети',
-		'ftrlinks_section_aboutus'	=> 'О #SITE_TITLE#',
+		'ftrlinks_section_general'	=> 'General links',
+		'ftrlinks_section_groups'	=> 'Groups',
+		'ftrlinks_section_findpeople'	=> 'Find More People',
+		'ftrlinks_section_aboutus'	=> 'About #SITE_TITLE#',
 		
-		'ftrlinks_sgn_dashboard'	=> 'Моя панель',
-		'ftrlinks_sgn_profile'		=> 'Мой профиль',
-		'ftrlinks_sgn_admin'		=> 'Администратор сети',
-		'ftrlinks_sgn_settings'		=> 'Настройки',
+		'ftrlinks_sgn_dashboard'	=> 'My Dashboard',
+		'ftrlinks_sgn_profile'		=> 'My Profile',
+		'ftrlinks_sgn_admin'		=> 'Network Administration',
+		'ftrlinks_sgn_settings'		=> 'Settings',
 		
-		'ftrlinks_sgr_mygroups'		=> 'Мои группы',
-		'ftrlinks_sgr_allgroups'	=> 'Все группы',
-		'ftrlinks_sgr_newgroup'		=> 'Создать группу',
+		'ftrlinks_sgr_mygroups'		=> 'My Groups',
+		'ftrlinks_sgr_allgroups'	=> 'All Groups',
+		'ftrlinks_sgr_newgroup'		=> 'Create New Group',
 		
-		'ftrlinks_sf_members'		=> 'Участники сети',
-		'ftrlinks_sf_invitemail'	=> 'Пригласить друга по email',
-		'ftrlinks_sf_invitelink'	=> 'Отпраить персональное приглашение',
+		'ftrlinks_sf_members'		=> 'Browse Network Members',
+		'ftrlinks_sf_invitemail'	=> 'Invite Coworkers via e-mail',
+		'ftrlinks_sf_invitelink'	=> 'Send personal invitation link',
 		
-		'os_ftrlinks_sf_members'		=> 'Смотреть участников сети',
-		'os_ftrlinks_sf_invitemail'		=> 'Пригласить людей по email',
-		'os_ftrlinks_sf_invitelink'		=> 'Отпраить персональное приглашение',
+		'os_ftrlinks_sf_members'		=> 'Browse Network Members',
+		'os_ftrlinks_sf_invitemail'		=> 'Invite People via e-mail',
+		'os_ftrlinks_sf_invitelink'		=> 'Send personal invitation link',
 		
-		'ftrlinks_sa_ourblog'		=> 'Официальный блог',
-		'ftrlinks_sa_faq'			=> 'FAQ',
-		'ftrlinks_sa_support'		=> 'Поддержка пользователей',
-		'ftrlinks_sa_terms'		=> 'Правила использования',
+		'ftrlinks_sa_ourblog'		=> 'Official Blog',
+		'ftrlinks_sa_faq'			=> 'Frequently Asked Questions',
+		'ftrlinks_sa_support'		=> 'Customer Support',
+		'ftrlinks_sa_terms'		=> 'Terms of Use',
 		
-		'os_ftrlinks_sa_ourblog'		=> 'Официальный блог',
-		'os_ftrlinks_sa_faq'			=> 'FAQ',
-		'os_ftrlinks_sa_support'		=> 'Контакт',
-		'os_ftrlinks_sa_terms'			=> 'Правила использования',
+		'os_ftrlinks_sa_ourblog'		=> 'Official Blog',
+		'os_ftrlinks_sa_faq'			=> 'Frequently Asked Questions',
+		'os_ftrlinks_sa_support'		=> 'Contact us',
+		'os_ftrlinks_sa_terms'			=> 'Terms of Use',
 	);
 	
 ?>
