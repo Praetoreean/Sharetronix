dd1b4aa42947b8b9e2496a599ca4f0740dd2efe0
Praetoreean <touche.sign@yahoo.com>
2017-10-13 19:30:33 +0300

    translated 13.10.17

--- system/languages/ru/inside/view.php
+++ system/languages/ru/inside/view.php
@@ -2,23 +2,23 @@
 	
 	$lang = array
 	(
-		'viewpost_hdr_prevpost'	=> 'Previous Post',
-		'viewpost_hdr_nextpost'	=> 'Next Post',
-		'viewpost_hdr_profile'	=> '#USERNAME#\'s profile',
+		'viewpost_hdr_prevpost'	=> 'Предыдущая запись',
+		'viewpost_hdr_nextpost'	=> 'Следующая запись',
+		'viewpost_hdr_profile'	=> 'Профиль #USERNAME#',
 		
-		'vpgroup_subtitle_type_public'	=> 'Public group',
-		'vpgroup_subtitle_type_private'	=> 'Private group',
-		'vpgroup_subtitle_nm_posts'	=> '#NUM# posts',
-		'vpgroup_subtitle_nm_posts1'	=> '1 post',
-		'vpgroup_subtitle_nm_members'	=> '#NUM# members',
-		'vpgroup_subtitle_nm_members1'	=> '1 member',
+		'vpgroup_subtitle_type_public'	=> 'Публичная группа',
+		'vpgroup_subtitle_type_private'	=> 'Приватная группа',
+		'vpgroup_subtitle_nm_posts'	=> '#NUM# записей',
+		'vpgroup_subtitle_nm_posts1'	=> '1 запись',
+		'vpgroup_subtitle_nm_members'	=> '#NUM# участников',
+		'vpgroup_subtitle_nm_members1'	=> '1 участник',
 		
-		'viewpost_attached_image'	=> 'Attached Image:',
-		'viewpost_attached_video'	=> 'Attached Video:',
+		'viewpost_attached_image'	=> 'Прикрепленные изображения:',
+		'viewpost_attached_video'	=> 'Прикрепленное видео:',
 		
 		'viewpost_date_format'		=> '%b %e %Y, %H:%M',
 		
-		'viewpost_comment_submit'	=> 'Add Comment',
+		'viewpost_comment_submit'	=> 'Комментировать',
 	);
 	
 ?>
